06653d7511e660ec39ae2efb21cc0417fdc661db
F. Perdomo <perdomofacundo@hotmail.com>
2018-09-28 15:23:05 -0300

    Add User profile photo on create

--- flags/app/models/user.rb
+++ flags/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_attached_file :profile, styles: {medium: "1280x720", thumb: "800x600", mini: "36x36"}
+  validates_attachment_content_type :profile, content_type: /\Aimage\/.*\Z/
 end
